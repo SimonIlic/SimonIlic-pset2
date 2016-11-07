5544ff0b4208254c73647d30041c9fa942fbd8ae
SimonIlic <simon.ilic@student.uva.nl>
2016-11-07 16:56:04 +0100

    Created next activity

---
+++ app/src/main/java/mprog/simon/simonilic_pset2/GetInputActivity.java
@@ -0,0 +1,13 @@
+package mprog.simon.simonilic_pset2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_input);
+    }
+}
--- app/src/main/java/mprog/simon/simonilic_pset2/StartActivity.java
+++ app/src/main/java/mprog/simon/simonilic_pset2/StartActivity.java
@@ -1,7 +1,9 @@
 package mprog.simon.simonilic_pset2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    /* handle begin button click */
+    public void forward(View view) {
+        Intent intent = new Intent(this, GetInputActivity.class);
+        startActivity(intent);
     }
 }
