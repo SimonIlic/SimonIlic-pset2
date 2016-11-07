a801bd6b7a5c3c32fbdefdb0f014804ec0edef32
SimonIlic <simon.ilic@student.uva.nl>
2016-11-07 16:17:38 +0100

    Initial commit

---
+++ app/src/main/java/mprog/simon/simonilic_pset2/StartActivity.java
@@ -0,0 +1,13 @@
+package mprog.simon.simonilic_pset2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
